8c47737330a1d24c191969c5b7670961adaaf21b
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-27 18:28:05 +0000

    Reestructuro componente

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,14 +1,12 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { AddContact } from "../component/addContact";
 
 
 export const Home = () => {
 	
 	return (
 	<div className="mt-2 me-4 d-flex justify-content-end">
-		<AddContact/>
+		
 	</div>
 	
 	
