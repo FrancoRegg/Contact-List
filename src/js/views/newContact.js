2f04db1d329afe7774ef797021a7efff53be753d
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-27 19:21:45 +0000

    Se guardan los valores de los inputs

--- src/js/views/newContact.js
+++ src/js/views/newContact.js
@@ -7,16 +7,20 @@ import "../../styles/newContact.css";
 
 export const NewContact = () => {
   const { store, actions } = useContext(Context);
+  const [contact, setContact] = useState({ name: "", last_name: "", phone: "", email: "", address: "" })
+  
+  console.log("CONTACTO", contact);
+  
 
   return (
     <div className="container ">
       <h1>agregar contacto</h1>
       <form className="new-contact">
-        <input type="text" name="name" placeholder="Nombre" className="form-control"/>
-        <input type="text" name="last name" placeholder="Apellido" className="form-control"/>
-        <input type="number" name="phone" placeholder="Telefono" className="form-control"/>
-        <input type="email" name="email" placeholder="Correo electronico" className="form-control"/>
-        <input type="text" name="address" placeholder="Direccion" className="form-control"/>
+        <input type="text" name="name" placeholder="Nombre" className="form-control" value={contact.name} onChange={(e)=>setContact({ name: e.target.value})}/>
+        <input type="text" name="last_name" placeholder="Apellido" className="form-control" value={contact.last_name} onChange={(e)=>setContact({ last_name: e.target.value})}/>
+        <input type="phone" name="phone" placeholder="Telefono" className="form-control" value={contact.phone} onChange={(e)=>setContact({ phone: e.target.value})}/>
+        <input type="email" name="email" placeholder="Correo electronico" className="form-control" value={contact.email} onChange={(e)=>setContact({ email: e.target.value})}/>
+        <input type="text" name="address" placeholder="Direccion" className="form-control" value={contact.address} onChange={(e)=>setContact({ address: e.target.value})}/>
       </form>
     </div>
   );
