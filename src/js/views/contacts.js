42a5ac10694a8b0cc4c7dc0c8d736db412f715a1
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-30 15:22:24 +0000

    traigo los contactos de la agenda y los pinto en pantalla

--- src/js/views/contacts.js
+++ src/js/views/contacts.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "../../styles/contacts.css";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
 
 export const Contacts = () => {
+  const { store, actions } = useContext(Context)
+  const [infoContact, setInfoContact] = useState([])
+
+
+  useEffect(() => {
+    fetch("https://playground.4geeks.com/apis/fake/contact/agenda/mis_cojones_33")
+      .then(resp => resp.json())
+      .then(data => setInfoContact(data))
+      .catch(error => console.log(error))
+  }, [])
 
   return (
-    <div className="">
+    <div className="container">
       <h1>TODOS LOS CONTACTOS</h1>
+      <div className="lista-contactos">
+        {infoContact.map((value, index) => (
+          <div className="datos-contactos" key={index}>
+            <strong className="nombre-contacto"><i className="fas fa-user"></i>{value.full_name}</strong>
+            <p className="telefono-contacto"><i className="fas fa-phone"></i>{value.phone}</p>
+            <p className="correo-contacto"><i className="fas fa-envelope"></i>{value.email}</p>
+            <p className="direccion-contacto"><i class="fas fa-map-pin"></i>{value.address}</p>
+          </div>
+        ))}
+      </div>
       <Link to="/add-contact">
-        <button>añadir contacto</button>
+        <button>nuevo contacto</button>
       </Link>
     </div>
   )
