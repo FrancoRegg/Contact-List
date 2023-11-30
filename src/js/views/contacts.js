4fe9244318873e6bb6245441719f05170e255284
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-30 17:40:19 +0000

    se muestran los datos del contactos y reubico la redireccion

--- src/js/views/contacts.js
+++ src/js/views/contacts.js
@@ -8,24 +8,30 @@ export const Contacts = () => {
   const { store, actions } = useContext(Context)
   const [infoContact, setInfoContact] = useState([])
 
+console.log("INFO CONT",infoContact);
 
-  useEffect(() => {
-    fetch("https://playground.4geeks.com/apis/fake/contact/agenda/mis_cojones_33")
-      .then(resp => resp.json())
-      .then(data => setInfoContact(data))
-      .catch(error => console.log(error))
+
+ useEffect(() => {
+  fetch("https://playground.4geeks.com/apis/fake/contact/agenda/mis_cojones_33")
+  .then(resp => resp.json())
+  .then(data => setInfoContact(data))
+  .catch(error => console.log(error))
   }, [])
+
 
   return (
     <div className="container">
       <h1>TODOS LOS CONTACTOS</h1>
       <div className="lista-contactos">
-        {infoContact.map((value, index) => (
-          <div className="datos-contactos" key={index}>
+        {infoContact.map((value) => (
+          <div className="datos-contactos" key={value.id}>
             <strong className="nombre-contacto"><i className="fas fa-user"></i>{value.full_name}</strong>
             <p className="telefono-contacto"><i className="fas fa-phone"></i>{value.phone}</p>
             <p className="correo-contacto"><i className="fas fa-envelope"></i>{value.email}</p>
             <p className="direccion-contacto"><i class="fas fa-map-pin"></i>{value.address}</p>
+            <Link to={`/edit-contact/${value.id}`}>
+              <button><i className="fas fa-pen"></i></button>
+            </Link>
           </div>
         ))}
       </div>
