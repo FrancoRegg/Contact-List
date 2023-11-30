138418c77dcc3a0bd946a6d9678ea12e79b43842
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-30 18:12:45 +0000

    uso setTimeOut para que de tiempo a que se ejecute el fetch de forma correcta

--- src/js/views/newContact.js
+++ src/js/views/newContact.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { Context } from "../store/appContext";
 
@@ -8,7 +8,7 @@ import "../../styles/newContact.css";
 export const NewContact = () => {
   const { store, actions } = useContext(Context);
   const [contact, setContact] = useState({ full_name: "", phone: "", email: "", address: "", agenda_slug: "mis_cojones_33" })
-
+  const navigate = useNavigate()
   console.log("CONTACTO", contact);
 
   const handleSubmint = (e) => {
@@ -22,6 +22,11 @@ export const NewContact = () => {
       .then(resp => resp.json())
       .then(data => setContact(data))
       .catch(error => console.log(error))
+      
+    setTimeout(() => {
+      navigate("/")
+    }, 1000)
+
   }
 
 
@@ -29,37 +34,37 @@ export const NewContact = () => {
     <div className="container ">
       <h1>agregar contacto</h1>
       <form className="new-contact" onSubmit={handleSubmint}>
-        <input 
-          type="text" 
-          name="full_name" 
-          placeholder="Nombre completo" 
-          className="form-control" 
-          value={contact.full_name} 
-          onChange={(e) => setContact({...contact, full_name: e.target.value })} 
+        <input
+          type="text"
+          name="full_name"
+          placeholder="Nombre completo"
+          className="form-control"
+          value={contact.full_name}
+          onChange={(e) => setContact({ ...contact, full_name: e.target.value })}
         />
-        <input 
-          type="phone" 
-          name="phone" 
-          placeholder="Telefono" 
-          className="form-control" 
-          value={contact.phone} 
-          onChange={(e) => setContact({...contact, phone: e.target.value })} 
+        <input
+          type="phone"
+          name="phone"
+          placeholder="Telefono"
+          className="form-control"
+          value={contact.phone}
+          onChange={(e) => setContact({ ...contact, phone: e.target.value })}
         />
-        <input 
-          type="email" 
-          name="email" 
-          placeholder="Correo electronico" 
-          className="form-control" 
-          value={contact.email} 
-          onChange={(e) => setContact({...contact, email: e.target.value })} 
+        <input
+          type="email"
+          name="email"
+          placeholder="Correo electronico"
+          className="form-control"
+          value={contact.email}
+          onChange={(e) => setContact({ ...contact, email: e.target.value })}
         />
-        <input 
-          type="text" 
-          name="address" 
-          placeholder="Direccion" 
-          className="form-control" 
-          value={contact.address} 
-          onChange={(e) => setContact({...contact, address: e.target.value })} 
+        <input
+          type="text"
+          name="address"
+          placeholder="Direccion"
+          className="form-control"
+          value={contact.address}
+          onChange={(e) => setContact({ ...contact, address: e.target.value })}
         />
         <button>Añadir contacto</button>
       </form>
