b5ded20a772abbb2543b4a1dfc1f91349d1a5329
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-12-01 19:16:53 +0000

    Ajuste de filas y columnas

--- src/js/views/contacts.js
+++ src/js/views/contacts.js
@@ -29,18 +29,23 @@ export const Contacts = () => {
       <h1 className="titulo">tus contactos</h1>
       <div className="lista-contactos">
         {store.contacts.map((value) => (
-          <div className="datos-contactos" key={value.id}>
+          <div className=" row datos-contactos" key={value.id}>
+            <div className="col-9">
             <strong className="nombre-contacto"><i className="fas fa-user"></i>{value.full_name}</strong>
             <p className="telefono-contacto"><i className="fas fa-phone"></i>{value.phone}</p>
             <p className="correo-contacto"><i className="fas fa-envelope"></i>{value.email}</p>
             <p className="direccion-contacto"><i className="fas fa-map-pin"></i>{value.address}</p>
+            </div>
+            <div className="col-3 botones-opciones">
             <Link to={`/edit-contact/${value.id}`}>
               <button className="boton-editar"><i className="fas fa-pen"></i></button>
             </Link>
             <button className="boton-eliminar" onClick={()=>confirmDeleteContact(value.id)}><i className="fas fa-trash"></i></button>
+            </div>
           </div>
         ))}
       </div>
+      <hr/>
       <Link to="/add-contact">
         <button className="boton-contacto">nuevo contacto</button>
       </Link>
