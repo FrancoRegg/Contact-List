d5a8360d867ffa21e69917ffc4c05a1ea44dd5df
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-12-01 10:14:14 +0000

    se establece la action para eliminar contacto segun su id

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,48 +12,33 @@ const getState = ({ getStore, getActions, setStore }) => {
 					if (!response.ok) {
 						throw new Error(`HTTP error! Status: ${response.status}`);
 					}
-					const data = response.json()
-					setStore({ contacts: data })
+					const data = await response.json()
+					setStore ({contacts:data})
 				} catch (error) {
 					console.log(error);
 				}
 
 			},
-
-
-
-
-
-
-
-
-
-
-
-
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			//Elimina un contacto segun su id
+			deleteContact: async (id) => {
+				try {
+					const response = await fetch(`https://playground.4geeks.com/apis/fake/contact/${id}`, {
+						method: 'DELETE',
+						headers: { "Content-Type": "application/json" }
+					})
+					if (!response.ok) {
+						throw new Error(`HTTP error! Status: ${response.status}`);
+					}
+					setStore((prevStore) => ({
+						...prevStore,
+						contacts: prevStore.contacts.filter((contact) => contact.id !== id),
+					}));
+				}
+				catch (error) {
+					console.log(error);
+				}
 			}
+
 		}
 	};
 };
