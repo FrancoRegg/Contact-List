9b363c645dd6bec51d02e0fa31793c070dbaa0de
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-12-01 10:13:42 +0000

    se recarga la lista actualizada de contactos cuando se acepta la eliminacion

--- src/js/views/contacts.js
+++ src/js/views/contacts.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import "../../styles/contacts.css";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
@@ -6,51 +6,38 @@ import { Context } from "../store/appContext";
 
 export const Contacts = () => {
   const { store, actions } = useContext(Context)
-  const [infoContact, setInfoContact] = useState([])
-
-  console.log("INFO CONT", infoContact);
+  
+  console.log("STORE",store.contacts);
 
 //Trae todos los contactos de una agenda en particular
   useEffect(() => {
-    actions.getContact();
+    actions.getContact()
   }, [])
 
-  const confirmDeleteContact = (id) => {
+  const confirmDeleteContact = (id) => { //Muestra opcion para confirmar o cancelar la eliminacion
     const confirmed = window.confirm("¿Estás seguro de que quieres eliminar este contacto?");
-    if (confirmed) {
-      deleteContact(id);
+    if (confirmed) { //Si se confirma, se ejecuta la siguente funcion
+      actions.deleteContact(id)
+      .then(()=>{  //Si se elimina el contacto, cargar la lista actualizada
+        actions.getContact() 
+      })
     }
   };
-
-  const deleteContact = (id) => {
-    fetch(`https://playground.4geeks.com/apis/fake/contact/${id}`, {
-      method: 'DELETE',
-      headers: {"Content-Type": "application/json"}
-    })
-      .then(resp => resp.json())
-      .then((data) => {
-        // Actualizar la lista de contactos después de la eliminación
-        setInfoContact(infoContact.filter((contact) => contact.id !== id));
-        console.log(data);
-      })
-      .catch(error => console.log(error))
-  }
-
 
   return (
     <div className="container">
       <h1>TODOS LOS CONTACTOS</h1>
       <div className="lista-contactos">
-        {infoContact.map((value) => (
+        {store.contacts.map((value) => (
           <div className="datos-contactos" key={value.id}>
             <strong className="nombre-contacto"><i className="fas fa-user"></i>{value.full_name}</strong>
             <p className="telefono-contacto"><i className="fas fa-phone"></i>{value.phone}</p>
             <p className="correo-contacto"><i className="fas fa-envelope"></i>{value.email}</p>
-            <p className="direccion-contacto"><i class="fas fa-map-pin"></i>{value.address}</p>
+            <p className="direccion-contacto"><i className="fas fa-map-pin"></i>{value.address}</p>
             <Link to={`/edit-contact/${value.id}`}>
               <button><i className="fas fa-pen"></i></button>
             </Link>
-            <button onClick={()=>confirmDeleteContact(value.id)}><i class="fas fa-trash"></i></button>
+            <button onClick={()=>confirmDeleteContact(value.id)}><i className="fas fa-trash"></i></button>
           </div>
         ))}
       </div>
