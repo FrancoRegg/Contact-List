0a5f9250e554ee26cd137ce7e239e5641a1ea748
FrancoRegg <francoreggiardo97@gmail.com>
2024-05-09 10:29:04 +0200

    Primeras rutas basicas de CRUD

--- back/src/app.js
+++ back/src/app.js
@@ -2,9 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//Mostrar contactos
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World Everybody!')
 })
+
+//Crear nuevo contacto
+app.post('/', (req, res) => {
+    res.send('Got a POST request')
+})
+
+//Modificar datos del contacto
+app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user')
+})
+
+//Eliminar contacto
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at /user')
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
