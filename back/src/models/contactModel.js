3dcd9fa606162ce31a99cceadc8cac5bc94b3e41
FrancoRegg <francoreggiardo97@gmail.com>
2024-05-24 21:01:51 +0200

    Estructura del modelo de los contactos

---
+++ back/src/models/contactModel.js
@@ -0,0 +1,52 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db";
+import user from "./userModel";
+
+const contact = sequelize.define('contact', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  apellido: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  telefono: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  foto: {
+    type: DataTypes.STRING,
+  },
+  direccion: {
+    type: DataTypes.STRING,
+  },
+  notas: {
+    type: DataTypes.TEXT,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'id'
+    },
+    onDelete: 'CASCADE'
+  },
+}, {
+  timestamps: true,
+});
+
+user.hasMany(contact, { foreignKey: 'userId' });
+contact.belongsTo(user, { foreignKey: 'userId' });
+
+export default contact
