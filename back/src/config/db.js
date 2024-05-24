16f9209e49e32c925ead5e6954d87d4d9ca16f2d
FrancoRegg <francoreggiardo97@gmail.com>
2024-05-24 21:01:20 +0200

    Configuracion de la conexion a la base de datos

--- back/src/config/db.js
+++ back/src/config/db.js
@@ -1,3 +1,17 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize'
 import 'dotenv/config'
+
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+});
+
+export const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
 
