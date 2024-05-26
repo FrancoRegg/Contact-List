0cb7c9a7b0176a51222436fc509560f95745c8ff
FrancoRegg <francoreggiardo97@gmail.com>
2024-05-26 19:54:23 +0200

    Servidor configurado

--- back/src/app.js
+++ back/src/app.js
@@ -1,10 +1,23 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
+import { connectDB, sequelize } from './config/db.js'
+import bodyParser from 'body-parser'
+import authRoutes from './routes/userRoute.js';
+import contactRoutes from './routes/contactRoute.js';
+
 
 const PORT = process.env.PORT || 3001
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 
+app.use('/api/auth', authRoutes);
+app.use('/api/contacts', contactRoutes);
 
+connectDB()
+
+sequelize.sync().then(() => console.log('Conectado a la Base de Datos'))
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
+  console.log(`App listening on port ${PORT}`)
 })
