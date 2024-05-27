7a076fb9120cba2963670ad1d91b23fe4355cb1a
FrancoRegg <francoreggiardo97@gmail.com>
2024-05-27 17:56:20 +0200

    Correccion en los datos que se toman para registrar un usuario

--- back/src/controllers/authController.js
+++ back/src/controllers/authController.js
@@ -6,21 +6,20 @@ import jwt from 'jsonwebtoken';
 //Crear una funcion para el registro de usuario
 export const register = async (req, res) => {
   //Tormar desde el front los datos del usuario y enviarlos a la base de datos
-  const { nombre, apellido, email, password } = req.body;
-
   try {
-    const existingUser = await User.findOne({ where: { email } });
+    const { nombre, apellido, email, password } = req.body
+    const existingUser = await User.findOne({ where: { email } })
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: 'User already exists' })
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ nombre, apellido, email, password: hashedPassword });
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = await User.create({ nombre, apellido, email, password: hashedPassword })
     if (user) {
-      res.status(201).json({ message: 'User registered successfully' });
+      res.status(201).json({ message: 'User registered successfully' })
     }
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: 'Server error' })
   }
 }
 
