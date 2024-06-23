4d7e3e005dda9b62b0e36b0a8d28220a86d72ca8
FrancoRegg <francoreggiardo97@gmail.com>
2024-06-23 17:01:31 +0200

    Ajusto el middleware para el manejo de la autenticacion

--- back/src/middleware/middlewareAuth.js
+++ back/src/middleware/middlewareAuth.js
@@ -2,19 +2,23 @@ import jwt from 'jsonwebtoken'
 import User from '../models/userModel.js';
 
 const middleware = async (req, res, next) => {
-  const token = req.header('Authorization').replace('Bearer ', '');
 
-  if (!token) {
+  const authHeader = req.headers['authorization']
+  if (!authHeader) {
     return res.status(401).json({ message: 'No token, authorization denied' });
   }
+  const token = authHeader.replace('Bearer ', '')
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findByPk(decoded.id);
+    const user = await User.findByPk(decoded.id);
+    if (!user) {
+      return res.status(404).json("User not found");
+    }
+    req.user = user;
     next();
   } catch (error) {
     res.status(401).json({ message: 'Token is not valid' });
   }
 };
-
 export default middleware
