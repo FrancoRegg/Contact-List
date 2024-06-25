a19fa124eddd252b664bdd9c86fd5e3d351b1933
FrancoRegg <francoreggiardo97@gmail.com>
2024-06-25 11:37:54 +0200

    Añado ruta para mostrar un contacto por ID

--- back/src/routes/userRoute.js
+++ back/src/routes/userRoute.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { register, login } from '../controllers/authController.js';
-import { getAllUser } from '../controllers/userController.js';
+import { getAllUsers, getUser } from '../controllers/userController.js';
+
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/users', getAllUser);
+router.get('/users', getAllUsers);
+router.get('/user/:id', getUser);
 
 export default router;
