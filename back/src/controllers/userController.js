ac1da09cfc88dd3a282474d4d2376905bfc20ce4
FrancoRegg <francoreggiardo97@gmail.com>
2024-06-25 11:36:54 +0200

    Añado funcion para traer usuario por su ID

--- back/src/controllers/userController.js
+++ back/src/controllers/userController.js
@@ -1,17 +1,27 @@
 import User from "../models/userModel.js";
 
 //Traer todos los usuarios
-export const getAllUser = async (req, res) => {
-  try {
-    const response = await User.findAll();
-    res.send(response);
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
+export const getAllUsers = async (req, res) => {
+    try {
+        const response = await User.findAll();
+        res.send(response);
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
 };
 
 //Traer un usuario por ID
+export const getUser = async (req, res) => {
+    try {
+        const { id } = req.params
+        const response = await User.findByPk(id)
+        res.send(response)
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+}
 
 //Actualizar un usuario por ID
+
 
 //Eliminar un usuarios por ID
