f04562758467834ef980fc272737d6117e6ad42f
FrancoRegg <francoreggiardo97@gmail.com>
2024-06-26 11:12:56 +0200

    Añado control de errores en la sincronizacion de la base de datos

--- back/src/app.js
+++ back/src/app.js
@@ -9,6 +9,7 @@ import contactRoutes from './routes/contactRoute.js';
 
 const PORT = process.env.PORT || 3001
 const app = express()
+
 app.use(cors())
 app.use(bodyParser.json())
 
@@ -17,7 +18,19 @@ app.use('/', contactRoutes);
 
 connectDB()
 
-sequelize.sync().then(() => console.log('Conectado a la Base de Datos'))
+/**sequelize.sync().then(() => console.log('Conectado a la Base de Datos'))
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
-})
+})*/
+
+sequelize.sync()
+  .then(() => {
+    console.log('Conectado a la Base de Datos');
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Error al conectar a la base de datos: ', err);
+  });
