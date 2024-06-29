e2125f3847821db54fa45af80af6c097e316475c
FrancoRegg <francoreggiardo97@gmail.com>
2024-06-29 16:24:31 +0200

    Reajusto archivo

--- back/src/models/contactModel.js
+++ back/src/models/contactModel.js
@@ -3,47 +3,47 @@ import { sequelize } from "../config/db.js";
 import User from "./userModel.js";
 
 const contact = sequelize.define('contact', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  nombre: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  apellido: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  telefono: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  foto: {
-    type: DataTypes.STRING,
-  },
-  direccion: {
-    type: DataTypes.STRING,
-  },
-  notas: {
-    type: DataTypes.TEXT,
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: User,
-      key: 'id'
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
     },
-    onDelete: 'CASCADE'
-  },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    apellido: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    telefono: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    foto: {
+        type: DataTypes.STRING,
+    },
+    direccion: {
+        type: DataTypes.STRING,
+    },
+    notas: {
+        type: DataTypes.TEXT,
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        },
+        onDelete: 'CASCADE'
+    },
 }, {
-  timestamps: true,
+    timestamps: true,
 });
 
 User.hasMany(contact, { foreignKey: 'userId' });
